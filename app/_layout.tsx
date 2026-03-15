25b8d7d19032b11959504383b40f0c17bf806329
Bryan Mangapit <bryanjesusmangapit477@gmail.com>
2026-03-15 23:24:16 +0800

    lesson_6 - Navigation types

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,64 @@
-import { Stack } from "expo-router";
+import MaterialCommunityIcons from "@expo/vector-icons/MaterialCommunityIcons";
+import { Tabs } from "expo-router";
 
 export default function RootLayout() {
   return (
-    <Stack>
-      <Stack.Screen name="index" options={{ headerShown: false }} />
-    </Stack>
+    // the reason we used the color value from tabBarIcon is this below, it will change automatically from the top level
+    <Tabs screenOptions={{ tabBarActiveTintColor: "red" }}>
+      <Tabs.Screen
+        name="index"
+        options={{
+          headerShown: false,
+          tabBarIcon: ({ color, size }) => {
+            return (
+              <MaterialCommunityIcons
+                name="format-list-bulleted"
+                size={size}
+                color={color}
+              />
+            );
+          },
+        }}
+      />
+      <Tabs.Screen
+        name="counter"
+        options={{
+          title: "Counter",
+          tabBarIcon: ({ color, size }) => {
+            return (
+              <MaterialCommunityIcons
+                name="clock-outline"
+                size={size}
+                color={color}
+              />
+            );
+          },
+        }}
+      />
+      <Tabs.Screen
+        name="idea"
+        options={{
+          title: "Idea",
+          tabBarIcon: ({ color, size }) => {
+            return (
+              <MaterialCommunityIcons
+                name="lightbulb-outline"
+                size={size}
+                color={color}
+              />
+            );
+          },
+        }}
+      />
+      {/* Using modals: */}
+      {/* <Stack.Screen
+        name="idea"
+        options={{
+          title: "Idea",
+          presentation: "modal",
+          animation: "slide_from_bottom",
+        }}
+      /> */}
+    </Tabs>
   );
 }
